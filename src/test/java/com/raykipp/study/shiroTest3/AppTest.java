1af59808743cb9f24e7ce9de8133ebdfd2ff65a7
wangxh <wangxh@wyn88.com>
2019-09-24 15:33:28 +0800

    init

---
+++ src/test/java/com/raykipp/study/shiroTest3/AppTest.java
@@ -0,0 +1,20 @@
+package com.raykipp.study.shiroTest3;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
